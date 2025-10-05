4be29921b01ff35d719c64a2d74c0bcae88219f5
Gustavo Ribeiro Carpanez <gustavocarpanez@hotmail.com>
2025-10-04 21:09:18 -0300

    teste

--- Turma.py
+++ Turma.py
@@ -1,2 +1,19 @@
 class Turma():
-    
+    def __init__(self, codigo, nome, professor, alunos=None):
+        self.codigo = codigo
+        self.nome = nome
+        self.professor = professor
+        self.alunos = alunos if alunos is not None else []
+
+    def adicionar_aluno(self, aluno):
+        self.alunos.append(aluno)
+
+    def remover_aluno(self, aluno):
+        if aluno in self.alunos:
+            self.alunos.remove(aluno)
+
+    def listar_alunos(self):
+        return [aluno.nome for aluno in self.alunos]
+
+    def __str__(self):
+        return f'Turma {self.codigo} - {self.nome}, Professor: {self.professor.nome}, Alunos: {len(self.alunos)}'
